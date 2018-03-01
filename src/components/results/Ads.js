301d04dac2213ae0ba41b8ac4e0a7c5a7ab7727d
Rebecca <angryhamster14@gmail.com>
2018-03-01 13:03:58 -0800

    Reformatted ads. Will add more in a sec.

--- src/components/results/Ads.js
+++ src/components/results/Ads.js
@@ -14,14 +14,16 @@ class Ads extends React.Component {
         this.setState(() => ({
             cards: [
                 {
-                    key: 1, name: "Paul O'Shannessy",
+                    name: "Paul O'Shannessy",
                     avatar_url: "https://avatars.githubusercontent.com/u/8445?v=3",
-                    company: "Facebook"
+                    title: "Dog and Cat walker",
+                    bio: "Sup dude I will walk your pet man"
                 },
                 {
-                    key: 2, name: "Ben Alpert",
+                     name: "Ben Alpert",
                     avatar_url: "https://avatars.githubusercontent.com/u/7585659?v=3",
-                    company: "Facebook"
+                    title: "Professional Animal Handler",
+                    bio: "Professional pet caretaker. 25 years of experience at the SF Zoo."
                 }
             ]
         }));
@@ -31,19 +33,22 @@ class Ads extends React.Component {
     render(){
         const searchStyle = {
             float: "right",
-            width: "35%"
+            width: "35%",
+            borderRadius: '5px'
           };
           const searchButtonStyle = {
-            float: "right"
+            float: "right",
+            borderRadius: '5px'
           };
         return(
             <div>
-                 <form>
-          Search for a specific name, location...
+            <form>
+             Search for a specific name, location...
             <button style= {searchButtonStyle} onClick={this.loadCards}  type="button">Search</button>
             <input style={searchStyle} type="text" placeholder="Enter a tag..." />
             
           </form> 
+          <br />
                 
                 <CardList cards={this.state.cards} />
 
--- src/components/results/Card.js
+++ src/components/results/Card.js
@@ -3,13 +3,29 @@ import {Link} from 'react-router';
 
 //A single card with a user profile pic and blurb
 const Card = (props) => {
+    const cardStyle = {
+        padding:'7px', 
+        border: '1.5px solid',
+        borderRadius: '10px',
+        borderColor: "#707070",
+        width:"100%"
+        
+      };
+
+      const imgStyle = {
+        borderRadius: '10px',
+        width: "100",
+        height: "100", 
+        float: 'left'
+      }
     return (
-        <div style={{padding:'5px', border:'1px solid', width:'300px'}}>
-            <img src={props.avatar_url} width="75" height="75" />
-            <div style={{ display: 'inline-block', marginLeft: 10 }}>
-                <div style={{ fontSize: '1.25em' }}>{props.name}</div>
-                <div>{props.company}</div>
-            </div>
+        <div style={cardStyle}>
+            <img src={props.avatar_url} style={imgStyle} />
+            <div > <center>
+                <div> <h2> {props.name}  </h2></div>
+                <div><h4> {props.title} </h4> </div>
+                <div><p> {props.bio} </p> </div> 
+                </center></div> 
         </div>
     );
 };
