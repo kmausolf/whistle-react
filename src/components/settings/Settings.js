b481a1bf8be304b1f51a637fd889c1227b19918d
Rebecca <angryhamster14@gmail.com>
2018-02-28 17:24:19 -0800

    Made a settings page, form doesn't process anything though

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router';
+import SettingsForm from './SettingsForm';
 
 class Settings extends React.Component {
   render() {
     return (
-        <p>Settings</p>
+      <div>
+        <div className="jumbotron"> 
+          <h1><center>Settings</center></h1>
+        </div>
+        <div className="text-center">
+           <SettingsForm /> 
+        </div>
+      </div>
     );
   }
 }
---
+++ src/components/settings/SettingsForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class Settings extends React.Component {
+  render() {
+    return (
+    <form>
+    <h3>Payment Method </h3>
+    <select>
+					<option value="credit">Credit/Debit Card</option>
+					<option value="paypal">PayPal</option>
+					<option value="venmo">Venmo</option>
+					<option value="crypto">Crypto Currency</option>
+		</select> <break> </break>
+    <h3> Colorblind Mode: </h3> <break> </break>
+				<select>
+					<option value="off">Off</option>
+          <option value="off">On</option>
+				</select> 
+        <h3>Push Notification:</h3> <break> </break>
+				<select>
+					<option value="off">Off</option>
+					<option value="on">On</option>
+				</select>
+        <br /> <br />
+        <input type="reset" value="Reset">
+        </input>
+        
+        <input type="submit" value="Submit">
+        </input>
+
+
+			
+    </form>
+    );
+  }
+}
+
+export default Settings;
