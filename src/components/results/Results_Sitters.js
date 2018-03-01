6b9a53e9ff84a428fe2046da62d72f56514589a1
Rebecca <angryhamster14@gmail.com>
2018-03-01 11:28:28 -0800

    Added search bar stuff to Results_Walkers and Results_Sitters. About to load profiles upon button press.

--- src/components/results/Results_Sitters.js
+++ src/components/results/Results_Sitters.js
@@ -2,9 +2,33 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class Results_Sitters extends React.Component {
+  
+
   render() {
+    const searchStyle = {
+      float: "right",
+      width: "35%"
+    };
+    const searchButtonStyle = {
+      float: "right"
+    };
     return (
-        <p>Results -- Sitters</p>
+      <div>
+        <div className="jumbotron">
+          <h1><center>Sitters</center></h1>
+          <h2> <center>Here are some available Sitters near you!</center> </h2>
+        </div>
+
+        <div>
+        
+          <form>
+          Search for a specific name, location...
+            <button style={searchButtonStyle} type="submit">Search</button>
+            <input style={searchStyle} type="text" placeholder="Enter a tag..." />
+            
+          </form> 
+        </div>
+      </div>
     );
   }
 }
--- src/components/results/Results_Walkers.js
+++ src/components/results/Results_Walkers.js
@@ -2,9 +2,32 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class Results_Walkers extends React.Component {
+  
   render() {
+    const searchStyle = {
+      float: "right",
+      width: "35%"
+    };
+    const searchButtonStyle = {
+      float: "right"
+    };
     return (
-        <p>Results -- Walkers</p>
+      <div>
+        <div className="jumbotron">
+          <h1><center>Walkers</center></h1>
+          <h2> <center>Here are some available Walkers near you!</center> </h2>
+        </div>
+
+        <div>
+        
+          <form>
+          Search for a specific name, location...
+            <button style={searchButtonStyle} type="submit">Search</button>
+            <input style={searchStyle} type="text" placeholder="Enter a tag..." />
+            
+          </form> 
+        </div>
+      </div>
     );
   }
 }
