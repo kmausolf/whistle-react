dd32a0d586a92e517dd0164520a20ef60f2effd3
Rebecca <angryhamster14@gmail.com>
2018-03-02 14:09:37 -0800

    Moved PetList calls into MyPets. Fixed button refreshing by making button type=button. Default button behavior is to submit, so we change that

--- src/components/profile/OwnerProfile.js
+++ src/components/profile/OwnerProfile.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router';
-import PetList from "./PetList";
+import MyPets from "./MyPets";
 class OwnerProfile extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = { pets: [{key:1, msg:"hey"}] };
-
-    this.addPet = this.addPet.bind(this);
-}
-
-addPet() {
-  this.setState((prevState) => ({
-    //pets: prevState.pets.push({"key":1,"msg":"hey"})
-    pets: [{"key":2,"msg":"grr"}]
-
-  }));
-}
-
+  
   render() {
     const borderStyle = {
       padding:'7px', 
@@ -44,35 +29,7 @@ addPet() {
           <textarea rows="5" cols="50" id="personal-bio"  placeholder="Enter Bio Here..."></textarea>
               </center> </form>
       </div>
-      <div style={borderStyle}>
-                <h2><center> My Pets </center> </h2>
-
-                <PetList pets={this.state.pets} />
-      </div>
-      <div style={borderStyle}>
-      <form> <center>
-                <h2>Add a new pet...</h2>
-
-				Pet Species:
-				<select id="species-selector">
-					<option value="dog">Dog</option>
-					<option value="cat">Cat</option>
-					<option value="bird">Bird</option>
-				</select><br/>
-				Name: <input type="text" id="pet-name" placeholder="Name"/><br/>
-				Breed: <input type="text" id="pet-breed" placeholder="Breed"/><br/>
-				Pet insurance No.: <input type="text" id="pet-insurance" placeholder="Pet insurance No"/><br/>
-				Medicines: <input type="text" id="medicines" placeholder="Medicines"/><br/>
-				Medical Conditions: <input type="text" id="medical-conditions" placeholder="Medical Conditions"/><br/>
-				Emergency Vet No.: <input type="text" id="emergency-vet" placeholder="Emergency Vet No."/><br/>
-				Walk Frequency: <input type="text" id="walk-frequency-times" placeholder="Number of times"/>
-				per <input type="text" id="walk-frequency-days" placeholder="days"/><br/>
-                Any other notes: <br /> <textarea rows="5" cols="50" id="pet-bio" placeholder="Enter Bio here..."></textarea> <br />
-				<button onClick={this.addPet}>Add Another Pet</button>
-       </center> </form>        
-      </div>
-
-      
+      <MyPets />
     </div>
     );
   }
