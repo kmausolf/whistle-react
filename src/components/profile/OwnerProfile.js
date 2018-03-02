1ff780a4cf8871f7808fc60f92c48e1a68547965
Rebecca <angryhamster14@gmail.com>
2018-03-01 21:00:44 -0800

    Finished frontend for profile. Will add pet-adding functionality tmr.

--- src/components/profile/OwnerProfile.js
+++ src/components/profile/OwnerProfile.js
@@ -27,6 +27,31 @@ class OwnerProfile extends React.Component {
           <textarea rows="5" cols="50" id="personal-bio"  placeholder="Enter Bio Here..."></textarea>
               </center> </form>
       </div>
+      <div style={borderStyle}>
+                <h2><center> My Pets </center> </h2>
+      </div>
+      <div style={borderStyle}>
+      <form> <center>
+                <h2>Add a new pet...</h2>
+
+				Pet Species:
+				<select id="species-selector">
+					<option value="dog">Dog</option>
+					<option value="cat">Cat</option>
+					<option value="bird">Bird</option>
+				</select><br/>
+				Name: <input type="text" id="pet-name" placeholder="Name"/><br/>
+				Breed: <input type="text" id="pet-breed" placeholder="Breed"/><br/>
+				Pet insurance No.: <input type="text" id="pet-insurance" placeholder="Pet insurance No"/><br/>
+				Medicines: <input type="text" id="medicines" placeholder="Medicines"/><br/>
+				Medical Conditions: <input type="text" id="medical-conditions" placeholder="Medical Conditions"/><br/>
+				Emergency Vet No.: <input type="text" id="emergency-vet" placeholder="Emergency Vet No."/><br/>
+				Walk Frequency: <input type="text" id="walk-frequency-times" placeholder="Number of times"/>
+				per <input type="text" id="walk-frequency-days" placeholder="days"/><br/>
+                Any other notes: <br /> <textarea rows="5" cols="50" id="pet-bio" placeholder="Enter Bio here..."></textarea> <br />
+				<button onclick="createPet()">Add Another Pet</button>
+       </center> </form>        
+      </div>
     </div>
     );
   }
