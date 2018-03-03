23686772ecec1ccbfe9646d4e9b899ea6521af30
kmausolf <kmausolf@ucsd.edu>
2018-03-02 17:30:21 -0800

    Very basic ctmain with state for request preferences

--- src/components/ctmain/CtMain.js
+++ src/components/ctmain/CtMain.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router';
+import WalkerPref from './WalkerPref';
 
 class CtMain extends React.Component {
   render() {
     return (
-        <p>Caretaker Main</p>
+      <div>
+        <div className="jumbotron">
+          <h1><center>Whistle!</center></h1>
+          <div className="text-center">
+            <WalkerPref />
+          </div>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/components/ctmain/WalkerPref.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Link} from 'react-router';
+import {RadioGroup,RadioButton} from 'react-radio-buttons';
+
+class WalkerPref extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {takingRequests: false};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({takingRequests: !this.state.takingRequests});
+        event.preventDefault();
+    }
+
+    render(){
+        const buttonStyle = {
+            width: "20vw",
+            marginTop: "1%"
+        };
+        const reqOnButton = (
+            <div>
+                <button className="btn btn-success" style={buttonStyle} onClick={this.handleChange}>
+                    I'm Taking Requests
+                </button>
+            </div>
+        );
+        const reqOffButton = (
+            <div>
+                <button className="btn btn-danger" style={buttonStyle} onClick={this.handleChange}>
+                    I'm Not Taking Requests
+                </button>
+            </div>
+        );
+
+        return(
+            <div>
+                <form>
+                    {this.state.takingRequests ? reqOnButton : reqOffButton}
+                </form>
+            </div>
+        );
+    }
+}
+
+export default WalkerPref;
