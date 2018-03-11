835a3fcd8284b84a9bf168dbaef9d5b91e891b3f
Rebecca <angryhamster14@gmail.com>
2018-03-10 17:59:00 -0800

    Gonna see if prev commit correctly obtained input

--- src/components/home/LoginForm.js
+++ src/components/home/LoginForm.js
@@ -4,16 +4,41 @@ import {Link} from 'react-router';
 class LoginForm extends React.Component {
     constructor(props){
         super(props);
-        this.state = {email: '', pass: ''};
+        this.state = {email: '', pass: ''}; //changes depending on button pressed
 
+        this.saveUser = this.saveUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event){
         //Whenever the input notices a change, update the state
+        /*
+        let user;
+        user.email = event.target.email;
+        user.pass = event.target.pass;
+        */
         this.setState({email: event.target.email, pass: event.target.pass});
+       
         event.preventDefault();
+        alert((this.state.email));
     }
+
+    saveUser(event) {
+        event.preventDefault();
+        alert(JSON.stringify(this.state.email));
+        if (!this.courseFormIsValid()) {
+          return;
+        }
+    
+
+    
+        this.props.actions.saveUser(this.state.user)
+          .then(() => this.redirect())
+          .catch(error => {
+            
+            this.setState({saving: false});
+          });
+      }
 
     render(){
         const buttonStyle = {
@@ -30,10 +55,12 @@ class LoginForm extends React.Component {
                     Don't have an account?  <Link to="/register">Register</Link>
                 </label>
                 <br />
+                <button onClick={this.saveUser} > ffffffff</button>
                 <Link 
                     className="btn btn-primary"
                     role="button"
                     to="/ownermain"
+                    onClick={this.state.saveUser}
                     style={buttonStyle}
                     >
                     Login as Owner
