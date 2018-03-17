e03b4a1677a300edb1ff73b0ca813bc335bf18a3
Rebecca <angryhamster14@gmail.com>
2018-03-16 18:19:19 -0700

    Localstorage saves the currently logged in user. Have an issue with registration, 'Type error state.filter is not a function'.

--- src/components/ownermain/OwnerMain.js
+++ src/components/ownermain/OwnerMain.js
@@ -2,16 +2,25 @@ import React from 'react';
 import {Link} from 'react-router';
 import DatePickerForm from './DatePickerForm';
 import SearchTypeForm from './SearchTypeForm';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as userActions from '../../actions/userActions';
+import toastr from 'toastr';
 
 
 class OwnerMain extends React.Component {
   render() {
+
+    let currUser = JSON.parse(localStorage.getItem('currUser'));
+    //const {user} = this.props;
+   // alert(JSON.stringify(currUser));
     return (
       <div>
       <div className="jumbotron">
         <h1><center>Whistle!</center></h1>
        
       </div>
+      <h2><center>Welcome {(currUser.firstName)}! </center></h2>
       <h2><center>When do you need a caretaker?</center></h2>
       <div className="ownerChoice">
            <center>
@@ -25,4 +34,17 @@ class OwnerMain extends React.Component {
   }
 }
 
-export default OwnerMain;
+
+function mapStateToProps(state, ownProps) {
+  return{
+      user: state.user
+  };
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+      actions: bindActionCreators(userActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(OwnerMain);
